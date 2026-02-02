a015b106e5bdd18c62bd9a257e08c4e656adb4a8
Uallace Macedo <uallace.macedo@outlook.com>
2026-02-02 13:33:37 -0300

    feat: add BaseUI view

---
+++ src/com/uallace/clinic/view/BaseUI.java
@@ -0,0 +1,36 @@
+package com.uallace.clinic.view;
+
+import java.util.Scanner;
+
+import com.uallace.clinic.exception.InputException;
+
+public abstract class BaseUI {
+  protected Scanner scan;
+
+  public BaseUI(Scanner scan) {
+    this.scan = scan;
+  }
+
+  protected abstract void start();
+  protected abstract void showMenu();
+
+  protected int readInt(String msg) {
+    System.out.print(msg);
+    String input = scan.nextLine().trim();
+
+    if (input.isBlank()) {
+      return 0;
+    }
+    
+    try {
+      return Integer.parseInt(input);
+    } catch (NumberFormatException e) {
+      throw new InputException("Por favor, informe um valor valido.");
+    }
+  }
+
+  protected String readString(String msg) {
+    System.out.print(msg);
+    return scan.nextLine().trim();
+  }
+}
