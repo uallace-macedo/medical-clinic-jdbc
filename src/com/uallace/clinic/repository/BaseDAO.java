87c8f0c130c8b4e6dc4765bb642aa99bab70e239
Uallace Macedo <uallace.macedo@outlook.com>
2026-02-02 00:59:17 -0300

    feat: add getDefaultLimit function

--- src/com/uallace/clinic/repository/BaseDAO.java
+++ src/com/uallace/clinic/repository/BaseDAO.java
@@ -4,9 +4,13 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public abstract class BaseDAO<T> implements DAO<T> {
-  protected static int queryLimit = 25;
+  protected int queryLimit = 25;
 
   protected Connection getConnection() throws SQLException {
     return ConnectionFactory.getConnection();
+  }
+
+  public int getDefaultLimit() {
+    return this.queryLimit;
   }
 }
