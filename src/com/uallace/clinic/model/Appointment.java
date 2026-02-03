bf5ad3b5a742482c80d23c27185e29b293922ddf
Uallace Macedo <uallace.macedo@outlook.com>
2026-02-03 14:16:19 -0300

    feat: add appointment status

--- src/com/uallace/clinic/model/Appointment.java
+++ src/com/uallace/clinic/model/Appointment.java
@@ -3,17 +3,19 @@ package com.uallace.clinic.model;
 import java.time.LocalDateTime;
 
 public class Appointment {
-  int id;
-  Doctor doctor_id;
-  Patient patient_id;
-  LocalDateTime appointment_date;
+  private int id;
+  private Doctor doctor_id;
+  private Patient patient_id;
+  private LocalDateTime appointment_date;
+  private AppointmentStatus status;
 
   public Appointment() {}
-  public Appointment(int id, Doctor doctor_id, Patient patient_id, LocalDateTime appointment_date) {
+  public Appointment(int id, Doctor doctor_id, Patient patient_id, LocalDateTime appointment_date, AppointmentStatus status) {
     this.id = id;
     this.doctor_id = doctor_id;
     this.patient_id = patient_id;
     this.appointment_date = appointment_date;
+    this.status = status;
   }
 
   public int getId() {
@@ -46,5 +48,13 @@ public class Appointment {
 
   public void setAppointmentDate(LocalDateTime appointment_date) {
     this.appointment_date = appointment_date;
+  }
+
+  public AppointmentStatus getAppointmentStatus() {
+    return status;
+  }
+
+  public void setAppointmentStatus(AppointmentStatus status) {
+    this.status = status;
   }
 }
