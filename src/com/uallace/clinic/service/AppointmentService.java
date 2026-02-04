b4480d1f027703cec4bc970b3a5af82e57bfae99
Uallace Macedo <uallace.macedo@outlook.com>
2026-02-03 23:29:08 -0300

    feat: add doctors & patient validations on save & update

--- src/com/uallace/clinic/service/AppointmentService.java
+++ src/com/uallace/clinic/service/AppointmentService.java
@@ -13,19 +13,32 @@ import com.uallace.clinic.util.Page;
 
 public class AppointmentService {
   private final AppointmentDAO appointmentDAO = new AppointmentDAO();
+  private final int minDiffSchedule = 15;
 
   public void save(Appointment appointment) {
     if (appointment.getDoctor() == null) throw new EntityException("O agendamento precisa ter um doutor.");
     if (appointment.getPatient() == null) throw new EntityException("O agendamento precisa ter um paciente.");
     if (appointment.getAppointmentDate() == null) throw new EntityException("O agendamento precisa ter um data e hora.");
-    if (appointment.getAppointmentDate().isBefore(LocalDateTime.now())) throw new EntityException("Nao eh possivel agendar para uma data retroativa.");
+    if (appointment.getAppointmentDate().isBefore(LocalDateTime.now())) throw new EntityException("Nao e possivel agendar para uma data retroativa.");
 
     List<Appointment> doctorAppointments = appointmentDAO.getOpenedDoctorAppointments(appointment.getDoctor().getId());
     for (Appointment doctorAppointment : doctorAppointments) {
       long diff = Duration.between(doctorAppointment.getAppointmentDate(), appointment.getAppointmentDate()).abs().toMinutes();
-      if (diff < 180) {
-        throw new BusinessException(String.format("O doutor ja tem um agendamento as %s. Deve haver uma diferenca minima de 3 horas.", 
-          doctorAppointment.getAppointmentDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"))
+      if (diff < minDiffSchedule) {
+        throw new BusinessException(String.format("O doutor ja tem um agendamento as %s. Deve haver uma diferenca minima de %d minutos.", 
+          doctorAppointment.getAppointmentDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")),
+          minDiffSchedule
+        ));
+      }
+    }
+
+    List<Appointment> patientAppointments = appointmentDAO.getOpenedPatientAppointments(appointment.getPatient().getId());
+    for (Appointment patientAppointment : patientAppointments) {
+      long diff = Duration.between(patientAppointment.getAppointmentDate(), appointment.getAppointmentDate()).abs().toMinutes();
+      if (diff < minDiffSchedule) {
+        throw new BusinessException(String.format("O paciente ja tem um agendamento as %s. Deve haver uma diferenca minima de %d minutos.", 
+          patientAppointment.getAppointmentDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")),
+          minDiffSchedule
         ));
       }
     }
@@ -49,7 +62,35 @@ public class AppointmentService {
     return new Page<>(appointments, page, size, hasNext);
   }
 
+  public List<Appointment> findAllOpenedDoctorAppointments(int id) {
+    return appointmentDAO.getOpenedDoctorAppointments(id);
+  }
+
   public void update(Appointment appointment) {
+    if (appointment.getAppointmentDate().isBefore(LocalDateTime.now())) throw new BusinessException("Nao e possivel agendar para uma data retroativa.");
+
+    List<Appointment> doctorAppointments = appointmentDAO.getOpenedDoctorAppointments(appointment.getDoctor().getId());
+    for (Appointment doctorAppointment: doctorAppointments) {
+      long diff = Duration.between(doctorAppointment.getAppointmentDate(), appointment.getAppointmentDate()).abs().toMinutes();
+      if (diff < minDiffSchedule) {
+         throw new BusinessException(String.format("O doutor ja tem um agendamento as %s. Deve haver uma diferenca minima de %d minutos.", 
+          doctorAppointment.getAppointmentDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")),
+          minDiffSchedule
+        ));
+      }
+    }
+
+    List<Appointment> patientAppointments = appointmentDAO.getOpenedPatientAppointments(appointment.getPatient().getId());
+    for (Appointment patientAppointment : patientAppointments) {
+      long diff = Duration.between(patientAppointment.getAppointmentDate(), appointment.getAppointmentDate()).abs().toMinutes();
+      if (diff < minDiffSchedule) {
+        throw new BusinessException(String.format("O paciente ja tem um agendamento as %s. Deve haver uma diferenca minima de %d minutos.", 
+          patientAppointment.getAppointmentDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")),
+          minDiffSchedule
+        ));
+      }
+    }
+
     appointmentDAO.update(appointment);
   }
 
